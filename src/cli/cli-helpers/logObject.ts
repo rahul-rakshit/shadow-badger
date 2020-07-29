ed0f1a2e8d9adb27bf9795af396aecb19bfbd308
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-07-29 18:08:40 +0200

    padEnd keys on logObject to format as two columns

--- src/cli/cli-helpers/logObject.ts
+++ src/cli/cli-helpers/logObject.ts
@@ -3,7 +3,17 @@ import colors from 'colors';
 export function logObject(modelObject: object, collectionName: string) {
   console.log(colors.bold.blue(`Found the following ${collectionName}:`));
 
+  const keyColumnWidth = findLongestKeyLength(modelObject) + 1;
+
   for (const [key, value] of Object.entries(modelObject)) {
-    console.log(colors.bold(key) + ':', value);
+    const formattedKey = `${key}:`.padEnd(keyColumnWidth);
+    console.log(colors.bold(formattedKey), value);
   }
+}
+
+function findLongestKeyLength(modelObject: object) {
+  return Object.keys(modelObject).reduce((accumulator, fieldKey) => {
+    if (fieldKey.length < accumulator) return accumulator;
+    return fieldKey.length;
+  }, 0);
 }
