d1628f6682f2479360f6b40e9dd54f5fe162026e
Rahul Rakshit <rahul.rakshit@posteo.net>
2020-08-30 16:59:06 +0200

    Add tests for viewing currency

--- src/cli/commands/view/currencies/viewCurrenciesCommand.ts
+++ src/cli/commands/view/currencies/viewCurrenciesCommand.ts
@@ -1,6 +1,5 @@
 import { program } from 'commander';
-import { currencyActions } from '../../../../entity/Currency/currencyActions';
-import { processUtil as $ } from '../../../cli-helpers/processUtil';
+import { viewCurrency } from './viewCurrency';
 
 export const viewCurrenciesCommand = program
   .command('currencies')
@@ -10,20 +9,4 @@ export const viewCurrenciesCommand = program
   .option('-c, --code, <code>', 'The currency code, eg. USD')
   .option('-n, --name <name>', 'The currency name, eg. US_Dollar')
   .option('-$, --symbol <symbol>', 'The currency symbol, eg. $')
-  .action(async (opts: { name?: string; code?: string; symbol?: string }) => {
-    try {
-      const allCurrencies = await currencyActions.findAll({ where: opts });
-      if (allCurrencies.length === 0) $.logAndExitNotFoundMessage('currency');
-
-      const loggable = allCurrencies.map((currency) => ({
-        id: currency.id,
-        name: currency.name,
-        code: currency.code,
-        symbol: currency.symbol
-      }));
-
-      $.logList(loggable, 'currencies', opts);
-    } catch (error) {
-      $.logAndExitOnSqlEngineError('view', 'currencies', error.message);
-    }
-  });
+  .action(viewCurrency);
---
+++ src/cli/commands/view/currencies/viewCurrency.ts
@@ -0,0 +1,31 @@
+import { processUtil as $ } from '../../../cli-helpers/processUtil';
+import { currencyActions } from '../../../../entity/Currency/currencyActions';
+import { parseDefinedOpts } from '../../../cli-helpers/parseDefinedOpts';
+import { Currency } from '../../../../entity/Currency/Currency-d';
+
+export async function viewCurrency(opts: {
+  id?: string;
+  name?: string;
+  code?: string;
+  symbol?: string;
+}) {
+  const { name, code, symbol } = opts;
+  const idString = opts.id;
+  const id = Number(idString);
+
+  if (!idString && !name && !code && !symbol) $.logAndExitNoFilterCriteria();
+
+  try {
+    const foundCurrency = id
+      ? await currencyActions.findOne(id)
+      : await currencyActions.findOne(undefined, {
+          where: parseDefinedOpts({ name, code, symbol })
+        });
+
+    if (!foundCurrency) $.logAndExitNotFoundMessage('currency', opts.id);
+
+    $.logObject(foundCurrency as Currency, 'currency');
+  } catch (error) {
+    $.logAndExitOnSqlEngineError('view', 'currency', error.message);
+  }
+}
